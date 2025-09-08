d1734d62653b8c125ec4e31966832342fcc3112c
Jesutomisin "Gunn" Ipinmoye <jipinmoye@alueducation.com>
2025-09-08 13:45:20 +0400

    Update ToastContext.js

--- components/ToastContext.js
+++ components/ToastContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState } from 'react';
-import { Toast } from '@/components/UIDesignSystem';
+import { Toast } from '../components/UIDesignSystem';
 
 const ToastContext = createContext();
 

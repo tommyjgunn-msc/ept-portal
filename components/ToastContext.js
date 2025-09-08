d973db434f6914451491e854bd0f6c1e58184acf
Jesutomisin "Gunn" Ipinmoye <jipinmoye@alueducation.com>
2025-09-08 13:00:03 +0400

    Create ToastContext.js

---
+++ components/ToastContext.js
@@ -0,0 +1,33 @@
+import { createContext, useContext, useState } from 'react';
+import { Toast } from '@/components/UIDesignSystem';
+
+const ToastContext = createContext();
+
+export const ToastProvider = ({ children }) => {
+  const [toasts, setToasts] = useState([]);
+
+  const addToast = (toast) => {
+    const id = Date.now();
+    setToasts(prev => [...prev, { ...toast, id }]);
+  };
+
+  const removeToast = (id) => {
+    setToasts(prev => prev.filter(toast => toast.id !== id));
+  };
+
+  return (
+    <ToastContext.Provider value={{ addToast }}>
+      {children}
+      {toasts.map(toast => (
+        <Toast
+          key={toast.id}
+          {...toast}
+          isVisible={true}
+          onClose={() => removeToast(toast.id)}
+        />
+      ))}
+    </ToastContext.Provider>
+  );
+};
+
+export const useToast = () => useContext(ToastContext);

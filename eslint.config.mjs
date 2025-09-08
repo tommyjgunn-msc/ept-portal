8369c16448646761401ee3993b60cdec3037bea8
Jesutomisin "Gunn" Ipinmoye <jipinmoye@alueducation.com>
2025-09-08 15:00:09 +0400

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,21 @@
-// eslint.config.mjs
-export default {
-  extends: [
-    'next/core-web-vitals'
-  ],
-  rules: {
-    '@typescript-eslint/no-unused-vars': 'off',
-    'react/no-unescaped-entities': 'off'
+import { FlatCompat } from "@eslint/eslintrc";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    }
   }
-};
+];
+
+export default eslintConfig;

c72fb426b1de53db3e6cbfecbcfe5a9e851bc112
Jesutomisin "Gunn" Ipinmoye <jipinmoye@alueducation.com>
2025-09-18 11:56:32 +0400

    Update testDatesConfig.js, new dates inputted

--- utils/testDatesConfig.js
+++ utils/testDatesConfig.js
@@ -1,16 +1,17 @@
 // This file contains the test dates and capacities for both regular and refugee tests
 export const regularDates = [
-    { date: 'Friday, 13 June', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
-    { date: 'Friday, 20 June', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
-    { date: 'Wednesday, 25 June', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
-    { date: 'Thursday, 3 July', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
-    { date: 'Friday, 18 July', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
-    { date: 'Friday, 25 July', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
-    { date: 'Wednesday, 30 July', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
-    { date: 'Friday, 8 August', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
-    { date: 'Friday, 22 August', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
-    { date: 'Monday, 25 August', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
-    { date: 'Saturday, 10 May', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
+    { date: 'Friday, 03 October', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
+    { date: 'Friday, 10 October', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
+    { date: 'Friday, 17 October', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
+    { date: 'Friday, 24 October', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
+    { date: 'Friday, 07 November', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
+    { date: 'Friday, 14 November', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
+    { date: 'Wednesday, 19 November', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
+    { date: 'Friday, 21 November', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
+    { date: 'Friday, 28 November', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
+    { date: 'Friday, 05 December', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
+    { date: 'Wednesday, 10 December', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
+    { date: 'Friday, 12 December', venues: 4, capacity: { withLaptop: 75, withoutLaptop: 25 } },
   ];
   
   export const refugeeDates = [
